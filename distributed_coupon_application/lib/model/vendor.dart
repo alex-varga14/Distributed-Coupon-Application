03e2b8606474f9bd93671b1d22c7485a683a46a0
amanda.olearczuk1 <amanda.olearczuk1@cs.ucalgary.ca>
2023-03-03 21:21:32 -0700

    Add constructors and toString() to models

--- distributed_coupon_application/lib/model/vendor.dart
+++ distributed_coupon_application/lib/model/vendor.dart
@@ -3,4 +3,18 @@ class Vendor {
   String country = "null";
   String city = "null";
   String vendorName = "null";
+
+  Vendor();
+
+  Vendor.create({
+    required this.id,
+    required this.country,
+    required this.city,
+    required this.vendorName,
+  });
+
+  @override
+  String toString() {
+    return "{id: $id, country: $country, city: $city, vendorName: $vendorName}";
+  }
 }
