a6b79522d03b8b85321066f4ddee478135df373a
amanda.olearczuk1 <amanda.olearczuk1@cs.ucalgary.ca>
2023-03-03 22:07:23 -0700

    Add homepage, coupon detail page, coupon add page

--- distributed_coupon_application/lib/main.dart
+++ distributed_coupon_application/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:distributed_coupon_application/pages/CouponFeedPage.dart';
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: CouponFeedPage(),
     );
   }
 }
---
+++ distributed_coupon_application/lib/ui/widgets/CouponWidget.dart
@@ -0,0 +1,77 @@
+import 'package:distributed_coupon_application/model/coupon.dart';
+import 'package:distributed_coupon_application/model/vendor.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'CouponDetailWidget.dart';
+
+class CouponWidget extends StatefulWidget {
+  final Coupon coupon;
+  final Vendor couponVendor;
+
+  const CouponWidget(
+      {super.key, required this.coupon, required this.couponVendor});
+
+  @override
+  State<CouponWidget> createState() => _CouponWidgetState();
+}
+
+class _CouponWidgetState extends State<CouponWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CouponDetailWidget(
+                coupon: widget.coupon, couponVendor: widget.couponVendor),
+          ),
+        ),
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue[100],
+          border: Border.all(color: Colors.black),
+        ),
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  widget.couponVendor.vendorName,
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  widget.coupon.title,
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  "Expiry Date: ${widget.coupon.expiryDate.toString().split(' ')[0]}",
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(width: 20),
+            const Image(
+              width: 170,
+              image: NetworkImage(
+                  'https://media.istockphoto.com/id/1254508881/photo/woman-holding-sale-shopping-bags-consumerism-shopping-lifestyle-concept.jpg?s=612x612&w=0&k=20&c=wuS3z6nPQkMM3_wIoO67qQXP-hfXkxlBc2sedwh-hxc='),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
