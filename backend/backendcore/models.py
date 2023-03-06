1b46fac2018eeedc531b8bac33918709a2c7c1e9
Vianney Nguyen <nguyenvianney@gmail.com>
2023-03-06 12:04:09 -0700

    Add notes

--- backend/backendcore/models.py
+++ backend/backendcore/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+###########################
+## Note: after creating a class here, run 
+#
+# python manage.py makemigrations
+# python manage.py migrate
+#
+# in order to sync the data models with the db. Otherwise there will be an error when trying to
+# run the program.
+###########################
+
 # Create your models here.
 
 # https://www.django-rest-framework.org/api-guide/fields/
