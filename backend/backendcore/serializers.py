82efc60565ba523f7aaaff76e4cd4a0eeee10ac3
simran2684 <simm2684@gmail.com>
2023-03-07 00:30:43 -0700

    Models and serializers

--- backend/backendcore/serializers.py
+++ backend/backendcore/serializers.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 from backendcore import models
 
 # https://www.django-rest-framework.org/api-guide/serializers/#specifying-which-fields-to-include
-class PlaceholderSerializer(serializers.ModelSerializer):
+class VendorSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.PlaceholderModel
-        fields = ["text", "number"]
+        model = models.Vendor
+        fields = ['vendorID', 'country', 'city', 'vendorName']
+
+class CouponSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Coupon
+        fields = ['couponID', 'vendorID', 'date', 'title', 'description', 'quantity','isMultiuse']
+
