fe3f115fcfc35af5256c6e2cf0f307d5b1d62c25
Vianney Nguyen <nguyenvianney@gmail.com>
2023-03-07 00:51:42 -0700

    Add extra RPC servers

--- backend/backendcore/sync/syncclient.py
+++ backend/backendcore/sync/syncclient.py
@@ -3,7 +3,9 @@ from backendcore.sync import grpc_client
 grpcClient = grpc_client.GRPCClient(
     [
         "localhost:50000",
-        "localhost:50001"
+        "localhost:50001",
+        "localhost:50002",
+        "localhost:50003",
     ]
 )
 
