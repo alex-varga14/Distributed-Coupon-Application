d25e4b49ceabf4877a6f193d28aed885989daabb
Vianney Nguyen <nguyenvianney@gmail.com>
2023-03-16 18:10:49 -0600

    Fix wrong host

--- backend/settings/instance-3.py
+++ backend/settings/instance-3.py
@@ -1,5 +1,13 @@
 from backendapp.settings import *
 
+REPLICAS = [
+    "http://3.145.15.144:8000",
+    "http://3.145.15.144:8001",
+    "http://3.145.15.144:8002",
+    "http://3.145.15.144:8003",
+]
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
